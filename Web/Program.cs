e0beab1ede43203d6fcff2202b5317d33f20b453
OldLi <wenju1991@163.com>
2018-10-22 14:03:09 +0800

    update

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Web
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+            .UseUrls("http://*:4040");
     }
 }
